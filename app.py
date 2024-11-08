b02e33102631ab42ea856d8fb1256ae348c325d7
bacht2509 <bachathanh1@gmail.com>
2024-11-08 15:49:41 +0700

    Updated

--- app.py
+++ app.py
@@ -67,6 +67,7 @@ def main():
             st.metric("52-Week Low", "%.2f" %min_52_week_low)
 
         print(data[data.index.duplicated()])
+        print(pd. __version__)
 
         st.dataframe(data[::-1], hide_index=True, use_container_width=True)
         
